7c935f01c341ed61269bf94625b72eb0114b9482
Felipe Moreira Botton <46912491+fmbotton@users.noreply.github.com>
2022-12-09 16:51:32 -0300

    Update rank.py
    troca do metodo para receber o comando

--- rank.py
+++ rank.py
@@ -5,6 +5,8 @@ import keyboard
 
 def upar():
     count = 0
+    print("Clicke no jogo em até 2 segundos.")
+    time.sleep(2)
     while (count < 49):
         ac.press('t')
         time.sleep(0.6)
@@ -14,12 +16,18 @@ def upar():
         ac.moveTo(892,428)
         ac.click()
         time.sleep(1)
-        count += 1  
+        count += 1
 
 def sair():
     exit()
 
 print("Pressione R para upar ou S para sair.")
-keyboard.add_hotkey('r', upar)
-keyboard.add_hotkey('s', sair)
-keyboard.wait()
+
+while True:
+    try:
+        if keyboard.is_pressed('r'):
+            upar()
+        if keyboard.is_pressed('s'):
+            sair()
+    except:
+        break
